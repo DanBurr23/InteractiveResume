527caa22860cc474f4a21551ef57711201cd4c65
DanBurr23 <danburrus@sbcglobal.net>
2016-11-15 16:46:45 -0800

    Add files via upload

--- js/helper.js
+++ js/helper.js
@@ -48,7 +48,8 @@ var HTMLschoolDegree = '<br> -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%<br></div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
-var HTMLschoolMinor = '<em><br>Minor: %data%</em>';
+/*var HTMLschoolMinor = '<em><br>Minor: %data%</em>';*/
+/*var HTMLschoolURL = '<br><a href="https://%data%/>"%data%"</a>';*/
 
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
